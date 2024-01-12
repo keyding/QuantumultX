32ebb064999399f4e4a48a23aea7c740dc97c504
Caven <cavenasdev@gmail.com>
2024-01-12 23:27:20 +0800

    feat: add xxyq

---
+++ xxyq/index.js
@@ -0,0 +1,17 @@
+/*************************************
+
+[rewrite_local]
+
+^https?:\/\/fastapi\.ukids\.cn\/ucapp\/sync$ url script-response-body https://raw.githubusercontent.com/keyding/QuantumultX/main/xxyq/index.js
+
+[mitm] 
+
+hostname = fastapi.ukids.cn
+
+************************************/
+
+const body = JSON.parse($response.body);
+
+body.data.children[0].nickName = body.data.child.nickName = "Caven's baby 🌟";
+
+$done({ body: JSON.stringify(body) });
