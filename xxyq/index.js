4ef70d04f4c6968bea0d521d11bef05a5e9c44d0
Caven <cavenasdev@gmail.com>
2024-01-13 00:26:47 +0800

    feat: udpate vip

--- xxyq/index.js
+++ xxyq/index.js
@@ -13,5 +13,6 @@ hostname = fastapi.ukids.cn
 const body = JSON.parse($response.body);
 
 body.data.children[0].nickName = body.data.child.nickName = "Caven's baby 🌟";
+body.data.user.vip = body.data.user.svip = 1
 
 $done({ body: JSON.stringify(body) });
