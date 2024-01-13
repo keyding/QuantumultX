51db5a4ea8989c729a1004498503577dfbf62dfb
Caven <cavenasdev@gmail.com>
2024-01-13 09:31:21 +0800

    feat: update player

---
+++ xxyq/play.js
@@ -0,0 +1,17 @@
+/*************************************
+
+[rewrite_local]
+
+^https?:\/\/fastapi\.ukids\.cn\/coreapp\/play\/video\/V9\/online url script-response-body https://raw.githubusercontent.com/keyding/QuantumultX/main/xxyq/play.js
+
+[mitm] 
+
+hostname = fastapi.ukids.cn
+
+************************************/
+
+const body = JSON.parse($response.body)
+
+body.data.preview = false
+
+$done({ body: JSON.stringify(body) })
