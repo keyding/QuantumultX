6981c83ad1b3348facb25359ec898b05801d0666
Caven <cavenasdev@gmail.com>
2024-01-13 09:59:37 +0800

    feat: update

--- xxyq/play.js
+++ xxyq/play.js
@@ -13,7 +13,5 @@ hostname = fastapi.ukids.cn
 const body = JSON.parse($response.body)
 
 body.data.playType = 1
-body.data.preview = false
-body.data.previewDur = null
 
 $done({ body: JSON.stringify(body) })
